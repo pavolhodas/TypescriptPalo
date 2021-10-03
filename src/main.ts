9e8d5dcf29bcce1c528f5d6709c7d6b933298c79
pavolhodas <pavolhodas4@gmail.com>
2021-10-03 14:17:59 +0200

    EqualFunction

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-function totalDistance(heightOfStep : number, lenghtOfStep : number, towerHeight : number){
-    console.log((towerHeight / heightOfStep) *(lenghtOfStep + heightOfStep ));
+function IsEqual(a : number, b : number, c : number){
+    if(a == b && c == b)
+        {
+            console.log(3);
+        }
+        else if(a == b || b == c || c == a)
+        {
+            console.log(2);
+        }
+        else
+        {
+            console.log(0);
+        }
 }
-totalDistance(0.3, 0.2, 25.0);
+IsEqual(0.3, 0.2, 0.2);
