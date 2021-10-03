8ccf26f0e017cab5dc74054a0dda8461f101081a
pavolhodas <pavolhodas4@gmail.com>
2021-10-03 21:15:41 +0200

    checkEnding

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,9 @@
-let a:number[]=new Array(5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243);
-let oddSum = 0;
-let evenSum = 0;
+function checkEnding(m: string, n: string) {
+    return m.endsWith(n);
+  
+  }
+  console.log(checkEnding("abv", "bv"));
 
-function warOddEven(a:number[]){
-    for(let i=0;i<a.length;i++){
-
-        if(a[i]%2 != 0){
-            oddSum += a[i]
-        }
-        else{
-            evenSum += a[i]
-        }
-    }
-    if(evenSum>oddSum)
-    {
-        console.log(evenSum-oddSum);
-    }
-    else{
-        console.log(oddSum-evenSum);
-    }
-}
-warOddEven(a);
+  interface String {    
+    endsWith(searchString: string, endPosition?: number): boolean;
+};
